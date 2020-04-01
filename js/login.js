2c258800c00f469ef9029270c7f02549dec75051
mj <hi.minjungkim@gmail.com>
2020-04-01 18:19:55 +0900

    Make JS code short

--- js/login.js
+++ js/login.js
@@ -2,19 +2,14 @@ const id = document.querySelector('.id');
 const password = document.querySelector('.pw');
 const loginBtn = document.getElementById('login-btn');
 
+function changeColor(e) {
+    id.value.length > 0 && password.value.length > 0 ? 
+            loginBtn.style.backgroundColor = "#0095F6" : 
+            loginBtn.style.backgroundColor = "#C5E1FA";
+}
 
-id.addEventListener('keyup', function(e) {
-    if (id.value.length > 0 && password.value.length > 0) {
-        loginBtn.style.backgroundColor = "#0095F6";
-    } else {
-        loginBtn.style.backgroundColor = "#C5E1FA";
-    }
-});
-
-password.addEventListener('keyup', function(e) {
-    if (id.value.length > 0 && password.value.length > 0) {
-        loginBtn.style.backgroundColor = "#0095F6";
-    } else {
-        loginBtn.style.backgroundColor = "#C5E1FA";
-    }
-});
+function init() {
+    id.addEventListener('keyup', changeColor)
+    password.addEventListener('keyup', changeColor);
+}
+init();
